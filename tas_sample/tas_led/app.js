8dbcbc29367a7fd51d9e936951ca1b014cc1072e
Changyong <e7217@naver.com>
2019-04-17 16:46:52 +0900

    print test messages...

--- tas_sample/tas_led/app.js
+++ tas_sample/tas_led/app.js
@@ -114,7 +114,7 @@ function on_receive(data) {
     }
 }
 
-function getRandomInt(min, max) { //min ~ max 사이의 임의의 정수 반환
+function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
